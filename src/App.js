cb602a34f6c3ad2ec5e6291e9e55a92fe82718ef
cuongkv <cuongkv@mkjgame.net>
2025-10-23 14:26:07 +0700

    update store name

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ function Header() {
           <div className="container header-container">
             <div className="header-left">
               <div className="logo-wrapper">
-                <img src={logoImg} alt="989 Productions" className="logo-img" />
-                <span className="studio-name">989 Productions</span>
+                <img src={logoImg} alt="Zerog Apps Studio" className="logo-img" />
+                <span className="studio-name">Zerog Apps Studio</span>
               </div>
             </div>
 
@@ -41,9 +41,9 @@ function Header() {
 function HomePage() {
   const apps = [
     {
-      name: 'TR270G',
+      name: 'Money Manager: Budget Planner',
       image: currency,
-      link: 'https://play.google.com/store/apps/details?id='
+      link: 'https://play.google.com/store/apps/details?id=com.moneymanager.finance.walletmanager.moneysaving'
     }
   ];
 
@@ -52,8 +52,8 @@ function HomePage() {
       <section id="about" className="section about">
         <div className="container about-row">
           <div className="about-text-container">
-            <h2 className="highlight-title">989 Productions – Welcome Aboard</h2>
-            <p className="about-text">989 Productions is a forward-thinking mobile app studio committed to crafting innovative, dependable, and easy-to-use applications. Our mission is to blend creativity with functionality, delivering solutions that span across casual gaming, productivity, lifestyle, and education. Each app we develop is carefully designed with user experience in mind — offering intuitive navigation, thoughtful details, and practical value — to make everyday life simpler, smarter, and more enjoyable for users around the globe.
+            <h2 className="highlight-title">Zerog Apps Studio – Welcome Aboard</h2>
+            <p className="about-text">Zerog Apps Studio is a forward-thinking mobile app studio committed to crafting innovative, dependable, and easy-to-use applications. Our mission is to blend creativity with functionality, delivering solutions that span across casual gaming, productivity, lifestyle, and education. Each app we develop is carefully designed with user experience in mind — offering intuitive navigation, thoughtful details, and practical value — to make everyday life simpler, smarter, and more enjoyable for users around the globe.
             </p>
           </div>
           <div className="about-img-container">
@@ -111,7 +111,7 @@ function App() {
         </Routes>
         <footer className="footer">
           <div className="container text-center">
-            <p>&copy; {new Date().getFullYear()} 989 Productions. All rights reserved.</p>
+            <p>&copy; {new Date().getFullYear()} Zerog Apps Studio. All rights reserved.</p>
           </div>
         </footer>
       </div>
